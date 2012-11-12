b74c3335e5bef6a541df7dcc4bfafc035645bc46
Ian Miell <ian.miell@gmail.com>
2012-11-12 12:20:54 +0000

    subject bug

--- simple_mailer.py
+++ simple_mailer.py
@@ -28,6 +28,8 @@ def main(args):
 		exit()
 	if args.subject == None:
 		subject = ""
+	else:
+		subject = args.subject
 	auth_base64 = base64.b64encode('\0'+sender+'\0'+password)
 	smtp_session(sender,auth_base64,recipient,subject,content)
 
